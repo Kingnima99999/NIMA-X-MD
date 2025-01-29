cdd9ff306e186cf2ab92d63a1fd738365b5ebc4a
Kingnima99999 <kingnima99999@gmail.com>
2025-01-29 10:44:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session is",  //session id put
+SESSION_ID: process.env.SESSION_ID || "~Sn5jACYT#fmVyNIkrvnXJ1vHRPQboXc2SSvQ-evAyZ9GEbK_YmZM",  //session id put
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/6nT5R2M/0ccb2d898e907180.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94740250759",
